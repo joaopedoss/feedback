f9466fc8b437703b3be8a4f7ae5cfff45a8fc3f7
joaopedoss <jpss06joao@gmail.com>
2024-06-04 21:44:42 +0000

    feat: instalaçao e implantaçao do react-router-dom e do styled-components

---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    homeBackgound: "#131518",
+    cardGradient: "gradial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, 100%)",
+    textColor: "#969FAD",
+    submitButtomcolor: "#FC7614",
+    white: "FFF",
+    feedBackButtomColor: "#262E38",
+    feedBackButtomText: "#7C8798"
+}
